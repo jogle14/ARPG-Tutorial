04b579468e39f97a8e80300b93a09dc5ac668e30
jogle14 <120553654+jogle14@users.noreply.github.com>
2023-04-03 19:44:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ARPG-Tutorial
+ learning from youtube series https://www.youtube.com/watch?v=MVWdIk6hwJk
---
+++ player/gamer_man.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+@export var speed = 80
+@onready var animations = $AnimationPlayer
+
+func handleInput():
+	var movementDirection = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = movementDirection * speed
+
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "Down"
+		if velocity.x < 0:
+			direction = "Left"
+		if velocity.x > 0:
+			direction = "Right"
+		if velocity.y < 0:
+			direction = "Up"
+		animations.play("walk" + direction)
+	
+
+func _physics_process(delta):
+	handleInput()
+	updateAnimation()
+	move_and_slide()
